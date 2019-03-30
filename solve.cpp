5c89be5729f12afac131a68d85b1e480e45a26b1
kuouugit <erickuo5124@gmail.com>
2019-03-30 18:32:56 +0800

    solve sudoku

--- solve.cpp
+++ solve.cpp
@@ -1,6 +1,16 @@
-int main()
-{
-    // finish your solve code...
+#include<iostream>
+#include"sudoku.h"
+using namespace std;
 
+int main() {
+    int map[81];
+    for(int i=0; i<81; i++) cin>>map[i];
+    Sudoku sudoku(map);
+
+	switch(sudoku.solve()){
+		case 0: printf("0\n"); break;
+		case 1: printf("1\n"); sudoku.print(); break;
+		case 2: printf("2\n"); break;
+	}
     return 0;
 }
